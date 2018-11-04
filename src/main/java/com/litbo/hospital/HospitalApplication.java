0c902f44b89aa5b049512651538e741a2c5adcec
litbo <973046515@qq.com>
2018-11-04 11:38:48 +0800

    test

---
+++ src/main/java/com/litbo/hospital/HospitalApplication.java
@@ -0,0 +1,12 @@
+package com.litbo.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalApplication.class, args);
+    }
+}
---
+++ src/test/java/com/litbo/hospital/HospitalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.litbo.hospital;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HospitalApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
