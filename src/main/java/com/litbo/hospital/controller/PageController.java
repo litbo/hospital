bde3b2f23c9ba8ad8672384de19e7db8759ed01e
litbo <973046515@qq.com>
2018-11-04 15:37:56 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/litbo/hospital/controller/PageController.java

--- src/main/java/com/litbo/hospital/controller/PageController.java
+++ src/main/java/com/litbo/hospital/controller/PageController.java
@@ -3,7 +3,6 @@ package com.litbo.hospital.controller;
 public class PageController {
     public static void main(String[] args) {
         System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
+
     }
 }
