13bee1c786cfb4844f175647f966558be1cae8db
马少鹏 <1293826968@qq.com>
2018-11-07 16:20:06 +0800

    添加dpf工具类注释

--- src/main/java/com/litbo/hospital/common/utils/poi/ExportPdfUtil.java
+++ src/main/java/com/litbo/hospital/common/utils/poi/ExportPdfUtil.java
@@ -14,8 +14,21 @@ import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 页面数据生成pdf的工具类
+ */
 public class ExportPdfUtil {
-    public static void exportExcel(HttpServletResponse response, String fileName, PdfData data) throws Exception {
+
+    /**
+     * 设置请求头
+     * @param response HttpServletResponse
+     * @param fileName 文件名
+     * @param data 需要显示在pdf中的属性值对象
+     * @throws IOException IO异常
+     * @throws DocumentException 文档异常
+     * @throws IllegalAccessException 无法访问对象属性异常
+     */
+    public static void exportExcel(HttpServletResponse response, String fileName, PdfData data) throws IOException, IllegalAccessException, DocumentException {
         // 告诉浏览器用什么软件可以打开此文件
         response.setHeader("content-Type", "application/vnd.ms-pdf");
         // 下载文件的默认名称
@@ -23,7 +36,15 @@ public class ExportPdfUtil {
         exportPdf(data, response.getOutputStream());
     }
 
-    private static void exportPdf(PdfData data, OutputStream out) throws Exception {
+    /**
+     * 数据存到pdf中并把pdf文件写入到输出流中
+     * @param data 需要显示在pdf中的属性值对象
+     * @param out 输出流
+     * @throws IOException IO异常
+     * @throws DocumentException 文档异常
+     * @throws IllegalAccessException 无法访问对象属性异常
+     */
+    private static void exportPdf(PdfData data, OutputStream out) throws DocumentException, IOException, IllegalAccessException {
         Document document = new Document();
         // A4纸张
         document.setPageSize(PageSize.A4);
@@ -53,24 +74,31 @@ public class ExportPdfUtil {
         document.close();
     }
 
-    private static Font setChineseFont() {
-        com.itextpdf.text.Font fontChinese = null;
-        try {
-            BaseFont bf = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", BaseFont.NOT_EMBEDDED);
-            fontChinese = new com.itextpdf.text.Font(bf, 12, Font.NORMAL);
-        } catch (DocumentException | IOException e) {
-            e.printStackTrace();
-        }
-        return fontChinese;
+    /**
+     * 设置字体样式主要针对pdf中无法显示中文
+     * @return Font 返回字体样式
+     * @throws IOException IO异常
+     * @throws DocumentException 文档异常
+     */
+    private static Font setChineseFont() throws IOException, DocumentException {
+        BaseFont bf = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", BaseFont.NOT_EMBEDDED);
+        return new Font(bf, 12, Font.NORMAL);
     }
 
-    private static List<String> getValue(Object obj) throws Exception {
-        Class clz = obj.getClass();
+    /**
+     * 通过反射获取Object中属性的值
+     *
+     * @param object 需要获取属性值的对象
+     * @return List<String> 返回属性值的列表
+     * @throws IllegalAccessException 无法访问对象属性异常
+     */
+    private static List<String> getValue(Object object) throws IllegalAccessException {
+        Class clz = object.getClass();
         Field[] field = clz.getDeclaredFields();
         List<String> values = new ArrayList<>();
         for (Field f : field) {
             f.setAccessible(true);
-            String value = (String) f.get(obj);
+            String value = (String) f.get(object);
             values.add(value);
         }
         return values;
--- src/main/java/com/litbo/hospital/common/utils/poi/PdfData.java
+++ src/main/java/com/litbo/hospital/common/utils/poi/PdfData.java
@@ -1,17 +1,25 @@
 package com.litbo.hospital.common.utils.poi;
 
 import lombok.Data;
-import org.apache.poi.ss.formula.functions.T;
 
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ * 用于生成pdf时的数据封装
+ */
 @Data
 public class PdfData implements Serializable {
-    // 表头
+    /**
+     * 表头
+     */
     private List<String> titles;
-    // 数据
+    /**
+     * dpf中的数据
+     */
     private List<?> rows;
-    // 标题
+    /**
+     * 文件标题
+     */
     private String name;
 }
