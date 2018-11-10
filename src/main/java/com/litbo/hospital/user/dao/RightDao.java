10b73427fabd6a2066d66df2a49aaa120dcef072
li66 <1041868497@qq.com>
2018-11-10 18:58:07 +0800

    修改sql

--- src/main/java/com/litbo/hospital/user/dao/RightDao.java
+++ src/main/java/com/litbo/hospital/user/dao/RightDao.java
@@ -2,6 +2,7 @@ package com.litbo.hospital.user.dao;
 
 import com.litbo.hospital.user.bean.Right;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -9,5 +10,12 @@ import java.util.List;
 @Mapper
 public interface RightDao {
 
+    @Select("SELECT ri.*,r.* " +
+            "FROM s_right AS ri " +
+            "INNER JOIN s_role_right AS rr " +
+            "ON ri.right_id =rr.right_id " +
+            "INNER JOIN s_role AS r " +
+            "ON r.role_id =rr.role_id " +
+            "WHERE r.role_name =#{roleName}")
     List<Right> getRightsByRolename(String roleName);
 }
