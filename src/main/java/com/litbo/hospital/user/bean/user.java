f3b23714b475fc161e313c6318ff553b7d4e0c38
li66 <1041868497@qq.com>
2018-11-10 17:57:06 +0800

    删除user.java

--- src/main/java/com/litbo/hospital/user/bean/user.java
+++ src/main/java/com/litbo/hospital/user/bean/user.java
@@ -1,4 +1,13 @@
 package com.litbo.hospital.user.bean;
 
-public class user {
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String userId;
+    private String userName;
+    private String userPwd;
+    private String bz;
+
 }
