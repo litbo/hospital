1888234a7636859727258e351120179b0950bfd2
cao <925454721@qq.com>
2018-11-29 20:34:52 +0800

    计划制定查询，维修合同添加查询

--- src/main/java/com/litbo/hospital/security/service/impl/JhZdServiceImpl.java
+++ src/main/java/com/litbo/hospital/security/service/impl/JhZdServiceImpl.java
@@ -1,5 +1,7 @@
 package com.litbo.hospital.security.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.litbo.hospital.result.Result;
 import com.litbo.hospital.security.bean.JhZd;
 import com.litbo.hospital.security.dao.JhZdDao;
@@ -11,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 /**
  * @author zjc
  * @create 2018-11-29 14:52
+ * 计划service层
  */
 @Service
 public class JhZdServiceImpl implements JhZdService {
@@ -18,6 +21,11 @@ public class JhZdServiceImpl implements JhZdService {
     @Autowired
     private JhZdDao jhZdDao;
 
+    /**
+     * 计划添加
+     * @param jhZd
+     * @return
+     */
     @Override
     @Transactional
     public Result addJhZd(JhZd jhZd) {
@@ -26,5 +34,15 @@ public class JhZdServiceImpl implements JhZdService {
             return Result.success();
         }
         return Result.error("添加失败");
+    }
+
+    /**
+     * 假话查询
+     * @return
+     */
+    public Result listJhZd(int pageNum,int pageSize){
+        PageHelper.startPage(pageNum,pageSize);
+        PageInfo<JhZd> pageInfo = new PageInfo<>(jhZdDao.jhzdList());
+        return Result.success(pageInfo);
     }
 }
