746d24402bab970482a4444f73d93a08b0ca4dcc
litbo <973046515@qq.com>
2018-11-29 17:56:21 +0800

    c

--- src/main/java/com/litbo/hospital/result/CodeMsg.java
+++ src/main/java/com/litbo/hospital/result/CodeMsg.java
@@ -8,6 +8,7 @@ public class CodeMsg {
     private String msg;
     public static CodeMsg SUCCESS = new CodeMsg(0,"success");//数据返回成功
     public static CodeMsg SERVER_ERROR = new CodeMsg(500100,"服务器端异常");
+    public static CodeMsg PARAM_ERROR = new CodeMsg(417100,"参数异常");
     private CodeMsg(int code, String msg) {
         this.code = code;
         this.msg = msg;
