f26fcd8c543d1ec783f3e392448288437c1ba9b6
Administrator <1092571147@qq.com>
2019-01-16 18:45:39 +0800

    选择表头

--- src/main/java/com/litbo/hospital/beneficial/controller/SelectController.java
+++ src/main/java/com/litbo/hospital/beneficial/controller/SelectController.java
@@ -1,6 +1,9 @@
 package com.litbo.hospital.beneficial.controller;
 
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.PageInfo;
 import com.litbo.hospital.result.Result;
 import com.litbo.hospital.supervise.service.BmService;
 import com.litbo.hospital.user.service.EqService;
@@ -21,14 +24,35 @@ public class SelectController {
 
 
     @RequestMapping(value = "/listSelect")
-    public Result listCbAccount(@RequestParam(value = "pageNum", required = false, defaultValue = "1") int pageNum,
+    public Result listSelect(@RequestParam(value = "pageNum", required = false, defaultValue = "1") int pageNum,
                                 @RequestParam(value = "pageSize", required = false, defaultValue = "10") int pageSize,
                                 String key) {
         if(key.equals("eq")) {
-            return Result.success(es.listShowEqs(pageNum, pageSize));
+            PageInfo date = es.listShowEqs(pageNum, pageSize);
+            return Result.success(date);
         }
         if(key.equals("bm")) {
-            return Result.success(bmService.getBmList(pageNum,pageSize));
+            PageInfo date = bmService.getBmList(pageNum,pageSize);
+            return Result.success(date);
+        }
+        else
+            return null;
+    }
+
+    @RequestMapping(value = "/listSelectCols")
+    public Result listSelectCols(@RequestParam(value = "pageNum", required = false, defaultValue = "1") int pageNum,
+                                @RequestParam(value = "pageSize", required = false, defaultValue = "10") int pageSize,
+                                String key) {
+        if(key.equals("eq")) {
+            PageInfo date = es.listShowEqs(pageNum, pageSize);
+            return Result.success(date);
+        }
+        if(key.equals("bm")) {
+
+            String jsonMessage = "[{'type': 'radio'}, "+"{field: 'bmName', title: '科室名称'}]";
+            JSONArray myJsonArray = JSONObject.parseArray(jsonMessage);
+            PageInfo date = new PageInfo(myJsonArray);
+            return Result.success(date);
         }
         else
             return null;
