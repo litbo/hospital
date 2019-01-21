acbcd6a378f4aa67eb017282759fa6882b7a5bc4
马少鹏 <1293826968@qq.com>
2019-01-21 14:13:55 +0800

    整合页面

---
+++ src/main/java/com/litbo/hospital/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.litbo.hospital;
+
+import com.litbo.hospital.common.utils.GetProjectAddressUtil;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/META-INF/resources/")
+                .addResourceLocations("classpath:/static/")
+                .addResourceLocations("file:"+ GetProjectAddressUtil.getAddress()+"/logo/");
+    }
+}
