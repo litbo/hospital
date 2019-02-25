3a55bc6c1ca4f996b1f1a080709b0dce99a53f79
马少鹏 <1293826968@qq.com>
2019-02-25 14:27:25 +0800

    ..

--- src/main/java/com/litbo/hospital/HospitalApplication.java
+++ src/main/java/com/litbo/hospital/HospitalApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * @author Administrator
+ */
 @SpringBootApplication
 @EnableSwagger2
 public class HospitalApplication {
@@ -11,38 +14,4 @@ public class HospitalApplication {
     public static void main(String[] args) {
         SpringApplication.run(HospitalApplication.class, args);
     }
-
-
-    /**
-     * http重定向到https
-     * @return -
-     */
-   /* @Bean
-    public TomcatServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint constraint = new SecurityConstraint();
-                constraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                constraint.addCollection(collection);
-                context.addConstraint(constraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(httpConnector());
-        return tomcat;
-    }
-
-    @Bean
-    public Connector httpConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        //Connector监听的http的端口号
-        connector.setPort(80);
-        connector.setSecure(false);
-        //监听到http的端口号后转向到的https的端口号
-        connector.setRedirectPort(443);
-        return connector;
-    }*/
 }
--- src/main/java/com/litbo/hospital/WebMvcConfig.java
+++ src/main/java/com/litbo/hospital/WebMvcConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * @author Administrator
+ */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
