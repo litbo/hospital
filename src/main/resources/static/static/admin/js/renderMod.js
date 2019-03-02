45242cdcd6984a700e922fc9289ea03ce2a54bd7
FU <aderx@qq.com>
2019-03-02 17:05:29 +0800

    fixed bugs

--- src/main/resources/static/static/admin/js/renderMod.js
+++ src/main/resources/static/static/admin/js/renderMod.js
@@ -508,6 +508,7 @@ $(function () {
                                     }
                                 }
                             });
+                            return false;
                         })
                     }
                 },
