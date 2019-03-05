f88cfe3ba83ab03f1001fd4dceab7d3bf43bbce5
li66 <1041868497@qq.com>
2019-03-05 09:23:10 +0800

    用户设置 角色设置页面 整合  查询语句修改

--- src/main/java/com/litbo/hospital/user/vo/SelectUserVo.java
+++ src/main/java/com/litbo/hospital/user/vo/SelectUserVo.java
@@ -2,36 +2,16 @@ package com.litbo.hospital.user.vo;
 
 import lombok.Data;
 
-
+@Data
 public class SelectUserVo {
 
     private String userId;
 
     private String roleId;
 
+    private String bmId;
+
     private String status;
 
-    public String getUserId() {
-        return userId;
-    }
 
-    public void setUserId(String userId) {
-        this.userId = "%"+userId+"%";
-    }
-
-    public String getRoleId() {
-        return roleId;
-    }
-
-    public void setRoleId(String roleId) {
-        this.roleId = "%"+roleId+"%";
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = "%"+status+"%";
-    }
 }
