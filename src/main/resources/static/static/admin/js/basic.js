eecb8e421bb825932bd1658f2b650324619559ce
FU <aderx@qq.com>
2019-03-16 23:35:09 +0800

    1:修改所有表格中的总数据条数代码 2：新增传递信息获取表单信息 3：修复welcome页链接打开BUG 4：大部完成文档修改

--- src/main/resources/static/static/admin/js/basic.js
+++ src/main/resources/static/static/admin/js/basic.js
@@ -598,8 +598,8 @@ function subUp(value, data, param) {
             error: function (er) {
                 putMsg({
                     alert: "提交失败，请重试！",
-                    error: "数据提交异常",
-                    log: er
+                    error: "数据提交异常,错误信息为:",
+                    log: er.responseJSON
                 });
                 //提交失败后执行函数
                 value.fine && value.fine(er);
@@ -655,6 +655,24 @@ function getTableValue(name, inClear) {
     });
     //返回表格数据
     return oData;
+}
+
+function getFormValue(data){
+    var inputValue = $("input[name=" + valus + "]").val();
+    if (inputValue) {
+        dataP[valus] = inputValue;
+    } else if ($("select[name=" + valus + "]").val()) {
+        dataP[valus] = $("select[name=" + valus + "]").val();
+    } else if ($("textarea[name=" + valus + "]").val()) {
+        dataP[valus] = $("textarea[name=" + valus + "]").val();
+    } else if ($("input[type=radio][name=" + valus + "]").val()) {
+        dataP[valus] = $("input[type=radio][name=" + valus + "]").val();
+    } else if ($("input[type=checkbox][name=" + valus + "]").val()) {
+        var $cks = $("input[type=checkbox][name=" + valus + "]");
+        if ($cks[0].checked === true) {
+            dataP[valus] = $cks.val();
+        }
+    }
 }
 
 //抽象出的简化判断，成立返回false 不成立则返回true
@@ -905,7 +923,7 @@ action = func = {
                     //}
                     //
                     if(vas.send){
-                        checkStatus && action.sendTo(vas.send,checkStatus,table);
+                        checkStatus && func.sendTo(vas.send,checkStatus,table);
                         return true;
                     }
 
@@ -1095,7 +1113,7 @@ action = func = {
                 console.log("======normalBegin=====",value);
                 //按钮匹配
                 if (obj.event === value.event) {
-                    action.sendTo(value,ck,table);
+                    func.sendTo(value,ck,table);
                 }
             });
         });
@@ -1121,12 +1139,24 @@ action = func = {
             }
             //其他需要扩充的数据
             if (value.adds !== undefined) {
-                for (var name in value.adds) {
-                    if (value.adds.hasOwnProperty(name)) {
-                        value.data[name] = value.adds[name]
+                console.log("adds = ",value.add);
+                if(Type(value.adds) === "json"){
+                    for (var name in value.adds) {
+                        if (value.adds.hasOwnProperty(name)) {
+                            value.data[name] = value.adds[name]
+                        }
+                    }
+                }else if(Type(value.adds) === "array"){
+                    for(var g=0;g<value.adds.length;g++){
+                        var $selVal = $("select[name=" + value.adds[g] + "]").val();
+                        if($selVal){
+                            value.data[value.adds[g]] = $selVal;
+                        }
                     }
                 }
+
             }
+            //console.log("拼接完成：",value.data);
             //强制以JSON格式发送数据
             value.contentType = "application/json";
             //提交成功回调函数
@@ -1142,15 +1172,15 @@ action = func = {
                         data: oData
                     });
 
-                    layer.msg("操作成功！",function (index) {
+                    layer.alert("操作成功！",function (index) {
                         //判断是否刷新页面
                         value.reload && window.location.reload();
+                        layer.close(index);
                     });
                 } else {
-                    layer.msg("操作失败！")
+                    layer.alert("操作失败！")
                 }
-                //不论成功关闭layer
-                layer.close(index);
+
             };
             //判断是否为删除数据
             if (value.confirm === false) {
