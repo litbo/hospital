351b3982841d5a2a9ecf05ea634587c602748ed5
li66 <1041868497@qq.com>
2019-04-08 16:54:50 +0800

    edit

--- src/main/resources/static/static/admin/js/basic.js
+++ src/main/resources/static/static/admin/js/basic.js
@@ -1126,6 +1126,52 @@ function tableFunc(fn) {
 }
 
 /**
+ * @todo 本地存储-cookie
+ * @tips 以下存储方式将在未来被删除，推荐使用layui-data等框架支持方式本地存储，详见文档
+ * @arguments name -> 操作的cookie名
+ *            value -> 操作的cookie值
+ *            options -> 操作的cookie的其他参数
+ * @return {string} -> 读取的cookie
+ */
+jQuery.cookie = function (name, value, options) {
+    if (typeof value !== 'undefined') {
+        options = options || {};
+        if (value === null) {
+            value = '';
+            options.expires = -1;
+        }
+        var expires = '';
+        if (options.expires && (typeof options.expires === 'number' || options.expires.toUTCString)) {
+            var date;
+            if (typeof options.expires === 'number') {
+                date = new Date();
+                date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
+            } else {
+                date = options.expires;
+            }
+            expires = '; expires=' + date.toUTCString();
+        }
+        var path = options.path ? '; path=' + (options.path) : '';
+        var domain = options.domain ? '; domain=' + (options.domain) : '';
+        var secure = options.secure ? '; secure' : '';
+        document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
+    } else {
+        var cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+};
+
+/**
  * @todo 动态添加JS
  * @tips 动态添加页面渲染所必须的基础函数<顺序不能更改>
  * @arguments
