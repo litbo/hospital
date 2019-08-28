dcebf8a273942eb38b9f720b394d70cb3a55b209
菁 <1768908548@qq.com>
2019-08-28 15:41:59 +0800

    Revert "预案后台等"
    This reverts commit e921d69d

---
+++ src/main/java/com/litbo/hospital/lifemanage/dao/SgPdMapper.java
@@ -0,0 +1,85 @@
+package com.litbo.hospital.lifemanage.dao;
+
+import com.litbo.hospital.lifemanage.bean.SgPd;
+import com.litbo.hospital.lifemanage.bean.SgPdJg;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface SgPdMapper {
+
+    /**
+     * 插入盘点扫描到的编号
+     *
+     * @return
+     */
+    @Insert({
+            "insert into sg_pd (pd_id ,pd_scan_id, pd_scsj, pd_czr, pd_jhid )",
+            "values (#{pdId,jdbcType=VARCHAR},#{pdScanId,jdbcType=VARCHAR},#{pdScsj ,jdbcType=VARCHAR}," +
+                    "#{pdCzr,jdbcType=VARCHAR},#{pdJhid,jdbcType=VARCHAR})"})
+    int insertPdId(SgPd record);
+
+    /**
+     * 插入科室盘点状态和完成时间
+     * @return
+     */
+    @Insert("insert into sg_pd_jg (pd_jgId,pd_wcsj,pd_bmzt) values (#{pdJgId ,jdbcType=VARCHAR},#{pdWcsj ,jdbcType=DATE}," +
+            "#{pdBmzt,jdbcType=VARCHAR})")
+    int insertPdZt(SgPdJg sgPdJg);
+
+    /**
+     * 查询pdScanId是否重复
+     * @param pdScanId
+     * @return
+     */
+    @Select("select pd_scan_id from sg_pd where pd_scan_id = #{pdScanId,jdbcType=VARCHAR}")
+    String selectScanId(String pdScanId);
+
+    /**
+     * 查询所有插入的扫描到的编号
+     * @param pdJhid
+     */
+    @Select("select pd_scan_id from sg_pd where pd_jhid = #{pdJhid,jdbcType=VARCHAR}")
+    List<String> selectAllData(String pdJhid);
+
+    /**
+     * 对应设备编号（品名）的设备名字
+     * @param pdSbbh
+     * @return
+     */
+    @Select("select eq_name from eq_info where eq_sbbh = #{pdSbbh,jdbcType=VARCHAR}")
+    String selectSbbhById(String pdSbbh);
+
+    /**
+     * 对应资产编号的设备名字
+     * @param pdZcId
+     * @return
+     */
+    @Select("select eq_name from eq_info where eq_zcbh = #{pdZcId,jdbcType=VARCHAR}")
+    String selectZcbhById(String pdZcId);
+
+    /**
+     * 通过科室id查出该科室下所有的设备名称
+     * @param bmId
+     * @return
+     */
+    @Select("select eq_name from eq_info where eq_bmid = #{bmId,jdbcType=VARCHAR}")
+    List<String> selectNameByBmId(String bmId);
+
+    /**
+     * 根据科室id查出该科室下所有的设备编号
+     */
+    @Select("select eq_sbbh from eq_info where eq_bmid = #{bmId,jdbcType=VARCHAR}")
+    List<String> selectSbbhByBmId(String bmId);
+
+    /**
+     * 根据科室id查出该科室下所有的资产编号
+     */
+    @Select("select eq_zcbh from eq_info where eq_bmid = #{bmId,jdbcType=VARCHAR}")
+    List<String> selectZcbhByBmId(String bmId);
+
+
+}
