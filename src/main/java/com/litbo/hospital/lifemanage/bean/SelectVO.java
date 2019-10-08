83f87642562d6d13520a0e444502ecf190e70dd5
61643 <616439896@qq.com>
2019-10-08 19:41:43 +0800

    Tsui

---
+++ src/main/java/com/litbo/hospital/lifemanage/bean/SelectVO.java
@@ -0,0 +1,50 @@
+package com.litbo.hospital.lifemanage.bean;
+
+public class SelectVO {
+
+    private String eqSbbh;
+    private String eqName;
+    private String bmName;
+
+    @Override
+    public String toString() {
+        return "SelectVO{" +
+                "eqSbbh='" + eqSbbh + '\'' +
+                ", eqName='" + eqName + '\'' +
+                ", bmName='" + bmName + '\'' +
+                '}';
+    }
+
+    public String getEqSbbh() {
+        return eqSbbh;
+    }
+
+    public void setEqSbbh(String eqSbbh) {
+        this.eqSbbh = eqSbbh;
+    }
+
+    public String getEqName() {
+        return eqName;
+    }
+
+    public void setEqName(String eqName) {
+        this.eqName = eqName;
+    }
+
+    public String getBmName() {
+        return bmName;
+    }
+
+    public void setBmName(String bmName) {
+        this.bmName = bmName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(bmName.equals(((SelectVO)obj).getBmName()))
+            return true;//这里以name为判定标准。
+        else {
+            return false;
+        }
+    }
+}
