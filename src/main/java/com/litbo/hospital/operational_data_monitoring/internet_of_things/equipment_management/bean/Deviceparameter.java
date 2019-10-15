c3f1c07a42cba3ac9007a9f8d5b2dd38e8959eb1
nichenhui <1770059954@qq.com>
2019-10-15 13:59:43 +0800

    Revert "设备联网设置页面"
    This reverts commit a4617124

--- src/main/java/com/litbo/hospital/operational_data_monitoring/internet_of_things/equipment_management/bean/Deviceparameter.java
+++ src/main/java/com/litbo/hospital/operational_data_monitoring/internet_of_things/equipment_management/bean/Deviceparameter.java
@@ -106,10 +106,4 @@ public class Deviceparameter {
     private String qulityNum;
 
     private String errorRate;
-
-    private String machineNumber;
-    private String maintainStatus;
-    private String softVerChange;
-    private String hardCloseFlag;
-
 }
